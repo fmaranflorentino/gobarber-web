f0aa331f7ea22f6695ca6a605bda7a9e6139d554
Flávio Florentino <florentino.flavioweb@gmail.com>
2020-03-31 15:30:23 -0300

    feat(layout/auth): add auth layout

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import logo from '~/assets/img/logo.svg';
 
 export default function SignIn() {
   return (
-    <h1>in</h1>
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form>
+        <Input name="email" type="emai" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </Form>
+    </>
   );
 }
