8952a1db6964221880740fe1d1bedd1a71d19b10
Flávio Florentino <florentino.flavioweb@gmail.com>
2020-03-31 22:01:55 -0300

    feat(react-toastify): add lib to show toast on error and go on

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,28 +1,33 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
-
+import { toast } from 'react-toastify';
 import history from '~/services/history';
 import api from '~/services/api';
 
-import { signInSuccess } from './actions';
+import { signInSuccess, signFailure } from './actions';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  const response = yield call(api.post, 'sessions', {
-    email,
-    password,
-  });
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
 
-  const { user } = response.data;
+    const { user } = response.data;
 
-  if (!user.provider) {
-    console.tron.error(`usuario não é prestador`);
-    return;
+    if (!user.provider) {
+      console.tron.error(`usuario não é prestador`);
+      return;
+    }
+
+    yield put(signInSuccess(user.token, user));
+
+    history.push('/dashboard');
+  } catch (error) {
+    toast.error('Falha na autenticação, verifique seus dados.');
+    yield put(signFailure());
   }
-
-  yield put(signInSuccess(user.token, user));
-
-  history.push('/dashboard');
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
--- src/styles/globals.js
+++ src/styles/globals.js
@@ -1,6 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
 
+import 'react-toastify/dist/ReactToastify.css';
+
 export default createGlobalStyle`
+
   * {
     box-sizing: border-box;
     outline: 0;
