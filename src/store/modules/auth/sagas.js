2ee269805565eaadad7e05ec2daefdae21530370
Flávio Florentino <florentino.flavioweb@gmail.com>
2020-04-01 10:54:18 -0300

    feat(singIn/token): persist token info on requests header

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -21,6 +21,8 @@ export function* signIn({ payload }) {
       return;
     }
 
+    api.defaults.headers.Authorization = `Bearer ${user.token}`;
+
     yield put(signInSuccess(user.token, user));
 
     history.push('/dashboard');
@@ -56,7 +58,18 @@ export function* signUp({ payload }) {
   }
 }
 
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
 export default all([
+  takeLatest('persist/REHYDRATE', setToken),
   takeLatest('@auth/SIGN_IN_REQUEST', signIn),
   takeLatest('@auth/SIGN_UP_REQUEST', signUp),
 ]);
