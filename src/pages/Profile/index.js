91867bc060442bcec9940f9e84a595038a0f3d1e
Flávio Florentino <florentino.flavioweb@gmail.com>
2020-04-01 20:13:29 -0300

    feat(auth/saga): add layout action

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -4,6 +4,7 @@ import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
 import { updateProfileRequest } from '~/store/modules/user/actions';
+import { signOut } from '~/store/modules/auth/actions';
 
 import AvatarInput from './AvatarInput';
 
@@ -15,6 +16,10 @@ export default function Profile() {
 
   function handleSubmit(data) {
     dispatch(updateProfileRequest(data));
+  }
+
+  function handleSignOut() {
+    dispatch(signOut());
   }
 
   return (
@@ -42,7 +47,7 @@ export default function Profile() {
         <button type="submit">Atualizar perfil</button>
       </Form>
 
-      <button type="button">Sair do GoBarber</button>
+      <button type="button" onClick={handleSignOut}>Sair do GoBarber</button>
     </Container>
   );
 }
